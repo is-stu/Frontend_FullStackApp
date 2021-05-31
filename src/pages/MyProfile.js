4ca2b55dae17ef725c11891433b28b9cb0bb4b1f
Stewar <stewar1020@gmail.com>
2021-05-30 20:57:49 -0500

    Last changes

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -2,19 +2,24 @@ import React, { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { connect } from 'react-redux';
 import { getUser, updateUser } from '../actions/authActions';
+import { useHistory } from 'react-router-dom';
 
 const MyProfile = ({ dispatch, email, userId, profile }) => {
+  const history = useHistory();
   const { register, handleSubmit } = useForm({
     defaultValues: {
       name: profile.name,
       lastName: profile.lastName,
       emailVisible: profile.alternativeEmail,
+      email: email,
     },
   });
   const onSubmit = (data) => {
     data.userId = userId;
     console.log(data);
     updateUser(data);
+    dispatch(getUser(userId));
+    history.push(`/`);
   };
 
   useEffect(() => {
@@ -34,7 +39,6 @@ const MyProfile = ({ dispatch, email, userId, profile }) => {
         <input
           id='email'
           type='email'
-          defaultValue={email}
           readOnly
           {...register('email')}
         />
